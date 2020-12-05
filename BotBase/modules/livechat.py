37f6c653a66c6923478f9f82ffff4b5ccdc97cd9
alsoGAMER <35269770+alsoGAMER@users.noreply.github.com>
2020-12-05 18:13:10 +0100

    Another livechat fix

--- BotBase/modules/livechat.py
+++ BotBase/modules/livechat.py
@@ -60,9 +60,7 @@ async def begin_chat(_, query):
             [[InlineKeyboardButton(JOIN_CHAT_BUTTON, f"join_{query.from_user.id}")]])
         user = get_user(query.from_user.id)
         tg_id, tg_uname, date, banned = user
-        text = USER_INFO.format(tg_id=tg_id,
-                                tg_uname='@' + tg_uname if tg_uname else 'null',
-                                date=date,
+        text = USER_INFO.format(tg_id=tg_id, tg_uname='@' + tg_uname if tg_uname else 'null', date=date,
                                 status=YES if banned else NO,
                                 admin='N/A')
         CACHE[query.from_user.id].append([])
@@ -95,8 +93,8 @@ async def update_admins_list(_, query):
             join_chat_button = InlineKeyboardMarkup(
                 [[InlineKeyboardButton(JOIN_CHAT_BUTTON, f"join_{query.from_user.id}")]])
             user = get_user(query.from_user.id)
-            uid, uname, date, banned = user
-            text = USER_INFO.format(uid=uid, uname='@' + uname if uname else 'null', date=date,
+            tg_id, tg_uname, date, banned = user
+            text = USER_INFO.format(tg_id=tg_id, tg_uname='@' + tg_uname if tg_uname else 'null', date=date,
                                     status=YES if banned else NO,
                                     admin='N/A')
             for admin in ADMINS:
