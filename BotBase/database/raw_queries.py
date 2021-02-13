f338ab32efcc47cdbb9b35b7222f2c28c575248b
alsoGAMER <alsogamer786@gmail.com>
2021-02-13 20:19:50 +0100

    database/raw_queries: Fixed ban/unban (closes #3)

--- BotBase/database/raw_queries.py
+++ BotBase/database/raw_queries.py
@@ -19,7 +19,7 @@ CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
                         tg_id INTEGER(20) PRIMARY KEY NOT NULL,
                         tg_uname VARCHAR(32) UNIQUE NULL DEFAULT 'null',
                         date TEXT NOT NULL,
-                        banned INTEGER NOT NULL DEFAULT 0);
+                        banned TINYINT(1) NOT NULL DEFAULT 0);
             """
 # endregion
 
@@ -27,7 +27,7 @@ CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
 DB_GET_USERS = "SELECT users.tg_id FROM users"
 DB_GET_USER = "SELECT * FROM users WHERE users.tg_id = %s"
 DB_SET_USER = "INSERT INTO users (tg_id, tg_uname, date, banned) VALUES (%s, %s, %s, %s)"
-DB_BAN_USER = "UPDATE users SET users.banned = FALSE WHERE users.tg_id = %s"
+DB_BAN_USER = "UPDATE users SET users.banned = TRUE WHERE users.tg_id = %s"
 DB_UNBAN_USER = "UPDATE users SET users.banned = FALSE WHERE users.tg_id = %s"
 DB_CHECK_BANNED = "SELECT users.banned FROM users WHERE users.tg_id = %s"
 DB_UPDATE_NAME = "UPDATE users SET users.tg_uname = %s WHERE users.tg_id = %s"
